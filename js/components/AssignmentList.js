ab989b59ec3e871dc70c3406807750531a4accf4
marchenkovby <marchenkovby@gmail.com>
2025-03-01 16:37:28 +0300

    End lesson 10

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -5,11 +5,28 @@ export default {
 
     template: `
         <section v-show="assignments.length">
-            <h2 class="font-bold mb-2">{{ title }}</h2>
+            <h2 class="font-bold mb-2">
+                {{ title }}
+                <span>({{ assignments.length }})</span>
+            </h2>
 
-            <ul class="border border-gray-600 divide-y divide-gray-600">
+            <div class="flex gap-2">
+                <!-
+                    v-on:click=""
+                -->
+                <button
+                    @click ="currentTag = tag" 
+                    v-for="tag in tags" 
+                    class="border rounded px-1 py- text-sm"
+                    :class="{
+                        'border-blue-500 text-blue-500': tag === currentTag
+                    }"
+                >{{ tag }}</button>
+            </div>
+
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                 <assignment
-                    v-for="assignment in assignments"
+                    v-for="assignment in filteredAssignments"
                     :key="assignment.id"
                     :assignment="assignment"
                 ></assignment>
@@ -20,5 +37,32 @@ export default {
     props: {
         assignments: Array,
         title: String
+    },
+
+    data() {
+        return {
+            currentTag: 'all'
+        };
+    },
+
+    computed: {
+        filteredAssignments() {
+            if (this.currentTag === 'all') {
+                return this.assignments;
+            }
+
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        },
+        // Вычислямое свойство
+        tags() {
+            // Возвращает новый массив, формирует его с помощью стрелочной функции,
+            // которую использует в качестве колбэк-функции в методе map.
+            // Колбэк-функция вызывается для каждого элемента массива и
+            // из возращаемых значение будет сформирован новый массив.
+            // Так как в теле фунции одно выражение, фигурные скобки и return можно опустить,
+            // функция неявно вернет результат.  
+            // (a) => { return a.tag}
+            return ['all', ...new Set(this.assignments.map((a) => a.tag))];
+        }
     }
 }
